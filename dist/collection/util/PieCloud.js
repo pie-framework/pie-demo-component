7cb4d540b8b82dee7e636bc8bd42e97c8c91c657
lakatosandrei <lakatosandrei@gmail.com>
2019-01-03 15:51:46 +0200

    chore(issues): fixed some issues with some elements that were not rendering

--- dist/collection/util/PieCloud.js
+++ dist/collection/util/PieCloud.js
@@ -3,12 +3,6 @@ const getEmptyConfigure = () => class extends HTMLElement {
     set model(_) {
     }
 };
-/**
- *
- * @param {Object<string,string>} elements elements to load from pie cloud service
- * @param {HTMLDocument} doc - the document to load the scripts
- * @param {string} base_url - default base url for cloud service
- */
 export function loadCloudPies(elements, doc, base_url = 'https://pits-dot-pie-dev-221718.appspot.com/bundles/') {
     const head = doc.getElementsByTagName('head')[0];
     const keys = Object.keys(elements);
@@ -27,7 +21,6 @@ export function loadCloudPies(elements, doc, base_url = 'https://pits-dot-pie-de
                     console.log('defining elements');
                     if (!customElements.get(elName)) {
                         customElements.define(elName, pie.Element);
-                        // This fixes some cases where the pie build service fails
                         pie.Configure = isFunction(pie.Configure) ? pie.Configure : getEmptyConfigure();
                         customElements.define(elName + '-config', pie.Configure);
                     }
