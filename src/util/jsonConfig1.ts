830d47b1aa26083d83ca95de5479817a4662f6bd
lakatosandrei <lakatosandrei@gmail.com>
2019-01-31 16:31:31 +0200

    v1.8.0

---
+++ src/util/jsonConfig1.ts
@@ -0,0 +1,45 @@
+export default {
+  "_id": "5c50554112a2e00e4feaca96",
+  "index": 0,
+  "guid": "5005b3e9-e947-4faf-b389-bf3bc2735cd0",
+  "isActive": false,
+  "balance": "$2,891.49",
+  "picture": "http://placehold.it/32x32",
+  "age": 33,
+  "eyeColor": "green",
+  "name": "Hardin Sargent",
+  "gender": "male",
+  "company": "PYRAMIS",
+  "email": "hardinsargent@pyramis.com",
+  "phone": "+1 (894) 498-3554",
+  "address": "838 Grove Place, Bodega, Connecticut, 6693",
+  "about": "Lorem est nulla officia fugiat amet tempor dolor proident magna officia exercitation sit nostrud magna. Nulla voluptate amet labore dolor dolore pariatur nulla laboris proident nostrud pariatur deserunt est minim. Qui dolor pariatur commodo ad deserunt est qui. Excepteur nostrud deserunt ex labore adipisicing voluptate. Eu culpa anim sunt velit veniam dolor irure.\r\n",
+  "registered": "2017-03-15T01:28:11 -02:00",
+  "latitude": 47.036331,
+  "longitude": 4.853763,
+  "tags": [
+    "amet",
+    "commodo",
+    "proident",
+    "aliquip",
+    "qui",
+    "deserunt",
+    "Lorem"
+  ],
+  "friends": [
+    {
+      "id": 0,
+      "name": "Eleanor Conrad"
+    },
+    {
+      "id": 1,
+      "name": "Prince Whitfield"
+    },
+    {
+      "id": 2,
+      "name": "Rosalind Herrera"
+    }
+  ],
+  "greeting": "Hello, Hardin Sargent! You have 5 unread messages.",
+  "favoriteFruit": "strawberry"
+}
---
+++ src/util/jsonConfig3.ts
@@ -0,0 +1,45 @@
+export default {
+  "_id": "5c505541941ffb97a7aeff98",
+  "index": 2,
+  "guid": "8c252660-71e2-4e79-91d7-45c919d1a4f4",
+  "isActive": true,
+  "balance": "$3,579.87",
+  "picture": "http://placehold.it/32x32",
+  "age": 33,
+  "eyeColor": "brown",
+  "name": "Alisa Goodwin",
+  "gender": "female",
+  "company": "SPEEDBOLT",
+  "email": "alisagoodwin@speedbolt.com",
+  "phone": "+1 (833) 526-2343",
+  "address": "472 Keen Court, Sexton, Oklahoma, 8507",
+  "about": "Lorem qui cillum amet nostrud est laboris anim commodo. Occaecat pariatur et minim cupidatat ad eiusmod dolore cupidatat culpa. Adipisicing aute consequat dolor labore id aute duis esse id adipisicing dolore. Laborum labore cillum pariatur occaecat anim nisi Lorem.\r\n",
+  "registered": "2017-05-14T12:07:37 -03:00",
+  "latitude": -89.219431,
+  "longitude": 39.787296,
+  "tags": [
+    "sunt",
+    "mollit",
+    "consequat",
+    "velit",
+    "magna",
+    "minim",
+    "commodo"
+  ],
+  "friends": [
+    {
+      "id": 0,
+      "name": "Lamb Albert"
+    },
+    {
+      "id": 1,
+      "name": "Fischer Kidd"
+    },
+    {
+      "id": 2,
+      "name": "Maxwell Hicks"
+    }
+  ],
+  "greeting": "Hello, Alisa Goodwin! You have 7 unread messages.",
+  "favoriteFruit": "banana"
+};
