10dc31890aa9f48d8288adaa7308fe9625995a68
ed eustace <ed.eustace@gmail.com>
2019-09-03 17:14:55 +0100

    change scope

--- src/util/PieCloud.ts
+++ src/util/PieCloud.ts
@@ -1,58 +1,61 @@
-import isFunction from 'lodash/isFunction';
-import { getPackageWithoutVersion } from './utils';
+import isFunction from "lodash/isFunction";
+import { getPackageWithoutVersion } from "./utils";
 
-const getEmptyConfigure = () => class extends HTMLElement{
-
-  set model(_) {
-
-  }
-};
+const getEmptyConfigure = () =>
+  class extends HTMLElement {
+    set model(_) {}
+  };
 
 /**
- * 
+ *
  * @param {Object<string,string>} elements elements to load from pie cloud service
- * @param {HTMLDocument} doc - the document to load the scripts 
- * @param {string} base_url - default base url for cloud service
+ * @param {HTMLDocument} doc - the document to load the scripts
+ * @param {string} baseUrl - default base url for cloud service
  */
 export function loadCloudPies(
-  elements, 
+  elements,
   doc,
-  base_url = 'https://pits-dot-pie-dev-221718.appspot.com/bundles/') {
-    const head = doc.getElementsByTagName('head')[0];
-    const keys = Object.keys(elements);
+  baseUrl = "https://pits-dot-pie-dev-221718.appspot.com/bundles/"
+) {
+  console.log("elements:", elements);
 
-    for (const key in keys) {
-      const elementName = keys[key];
-      const npmPackage:string = elements[elementName];
-      const packageWithoutVersion = getPackageWithoutVersion(npmPackage);
-      const script = doc.createElement('script');
-      const onloadFn = (_package => {
-        return () => {
-          const packages = _package.split('+');
-          const elementsName = elementName.split('+');
+  const head = doc.getElementsByTagName("head")[0];
+  const keys = Object.keys(elements);
 
-          packages.forEach((pack, index) => {
-            const pie = window['pie'].default[pack];
-            const initialEl = elementsName[index];
-            const atSymbolPos = initialEl.indexOf('@');
-            const elName = atSymbolPos >= 0 ? initialEl.slice(0, atSymbolPos) : initialEl;
-            console.log('defining elements');
+  for (const key in keys) {
+    const elementName = keys[key];
+    const npmPackage: string = elements[elementName];
+    const packageWithoutVersion = getPackageWithoutVersion(npmPackage);
+    const script = doc.createElement("script");
+    const onloadFn = (_package => {
+      return () => {
+        const packages = _package.split("+");
+        const elementsName = elementName.split("+");
 
-            if (!customElements.get(elName)) {
-              customElements.define(elName, pie.Element);
+        packages.forEach((pack, index) => {
+          const pie = window["pie"].default[pack];
+          const initialEl = elementsName[index];
+          const atSymbolPos = initialEl.indexOf("@");
+          const elName =
+            atSymbolPos >= 0 ? initialEl.slice(0, atSymbolPos) : initialEl;
+          console.log("defining elements");
 
-              // This fixes some cases where the pie build service fails
-              pie.Configure = isFunction(pie.Configure) ? pie.Configure : getEmptyConfigure();
+          if (!customElements.get(elName)) {
+            customElements.define(elName, pie.Element);
 
-              customElements.define(elName + '-config', pie.Configure);
-            }
-          });
-        };
-      })(packageWithoutVersion);
+            pie.Configure = isFunction(pie.Configure)
+              ? pie.Configure
+              : getEmptyConfigure();
 
-      script.id = elementName;
-      script.onload = onloadFn;
-      script.src = base_url + npmPackage + '/editor.js';
-      head.appendChild(script);
+            customElements.define(elName + "-config", pie.Configure);
+          }
+        });
+      };
+    })(packageWithoutVersion);
+
+    script.id = elementName;
+    script.onload = onloadFn;
+    script.src = baseUrl + npmPackage + "/editor.js";
+    head.appendChild(script);
   }
 }
